184423fa5fa09eb00b186d1f82f0bb9c04206542
Sally <su@ucsd.edu>
2018-01-25 10:50:04 -0800

    more changes to javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,37 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("I love cookies");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+		
+
 	});
+	$("a.thumbnail").click(projectClick);
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+
+
+ 
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(".project-description").toggle();
+    }
+}
+
+
+}
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
-}
